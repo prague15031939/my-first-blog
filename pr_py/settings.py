732ef252e76263162abaf8da8d40386cf9364320
Antoha <mario.lazer@mail.ru>
2018-10-29 22:56:33 +0300

    Changed the HTML for the site.

--- pr_py/settings.py
+++ pr_py/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm0wc=sg6i+#di4czn(f#&xc#_*!mhzktjjdgbt9jhb^t(m6d)0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '<keks>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'keks.pythonanywhere.com']
 
 
 # Application definition
